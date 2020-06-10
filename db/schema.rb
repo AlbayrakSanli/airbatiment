588db3617ce0296a6d2330295fb152224caf7157
Albayrak Furkan <albayrak.mo@hotmail.fr>
2020-06-10 19:19:58 +0200

    teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_10_150742) do
-
+ActiveRecord::Schema.define(version: 2020_06_10_163704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +58,11 @@ ActiveRecord::Schema.define(version: 2020_06_10_150742) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_owners_on_email", unique: true
     t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
