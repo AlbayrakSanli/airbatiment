c33e6517ebdffb03651b0b9163102338d53704ce
Albayrak Furkan <albayrak.mo@hotmail.fr>
2020-06-11 20:04:58 +0200

    CRUD houses

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,4 +1,6 @@
 class HousesController < ApplicationController
+  before_action :authenticate_owner!, except: [:index, :show]
+
   def index
     @houses = House.all
   end
@@ -10,18 +12,64 @@ class HousesController < ApplicationController
 
   def edit
     @house = House.find(params[:id])
+    @cities = City.all
   end
 
   def new
     @house = House.new
+    @cities = City.all
   end
 
   def create
+    @house = House.new(
+      title: params[:title],
+      description: params[:description],
+      price_house: params[:price_house],
+      price_visit: params[:price_visit],
+      area: params[:area],
+      room: params[:room],
+      owner: current_owner,
+      city: City.find_by(name: params[:city])
+    )
+    if @house.save
+      redirect_to root_path
+      flash[:success] = "La maison a bien été modifié!"
+    else
+      flash[:danger] = "La saisie n'a pas un format valide, merci de saisir à nouveau"
+      redirect_to new_house_path
+    end
   end
 
   def update
+    @house = House.find(params[:id])
+    if @house.update(
+      title: params[:title],
+      description: params[:description],
+      price_house: params[:price_house],
+      price_visit: params[:price_visit],
+      area: params[:area],
+      room: params[:room],
+      owner: current_owner,
+      city: City.find_by(name: params[:city])
+    )
+      redirect_to root_path
+      flash[:success] = "La maison a bien été modifié!"
+    else
+      flash[:danger] = "La saisie n'a pas un format valide, merci de saisir à nouveau"
+      redirect_to house_path
+    end
   end
 
   def destroy
+    House.find(params[:id]).delete
+    redirect_to root_path
   end
+
+  private
+
+  def authenticate_owner!
+		unless owner_signed_in?
+			redirect_to new_owner_session_path
+		end
+	end
 end
