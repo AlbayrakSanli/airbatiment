aa97a1b58422f0b9176549239e46fcef898bd735
jeremyhallr <jeremy.h8@hotmail.fr>
2020-06-11 09:48:04 +0200

    Add users controller, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :houses
   root "houses#index"
   resources :teams, only: :index
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
