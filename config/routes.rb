34a28d94123b08d8902c47ad934c8324517da256
jeremyhallr <jeremy.h8@hotmail.fr>
2020-06-11 10:08:31 +0200

    Add owners controller, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root "houses#index"
   resources :teams, only: :index
   resources :users
+  resources :owners
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
