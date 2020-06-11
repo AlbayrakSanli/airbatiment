6be16c3755f550ceaff204e5d121eb1a78e5998f
jeremyhallr <jeremy.h8@hotmail.fr>
2020-06-11 14:01:41 +0200

    Add and plug Stripe

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :users
   resources :owners
   resources :appointments
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
