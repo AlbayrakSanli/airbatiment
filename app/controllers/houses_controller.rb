304289a982a321cb0a66e818852629aa96be243d
Lebeil <liedel75@gmail.com>
2020-06-11 21:44:29 +0200

    fix controller house new edit

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -19,12 +19,32 @@ class HousesController < ApplicationController
   end
 
   def create
+    @house = House.new(post_params)
+    if @house.save
+      flash[:success] = 'Maison créée !'
+      render 'houses/show'
+    else
+      flash[:alert] = 'erreur'
+      render 'houses/new'
+    end
   end
 
   def update
+    @house = House.find(params[:id])
+    if @house.update(post_params)
+      flash[:success] = "Mis à jour ok!"
+      render 'houses/show'
+    else
+      flash[:alert] = 'erreur'
+      render 'houses/edit'
+    end
   end
 
   def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    flash[:danger] = 'Supprimé !'
+    render 'index'
   end
 
   def authenticate_owner
@@ -33,4 +53,11 @@ class HousesController < ApplicationController
       redirect_to new_owner_session_path
     end
   end
+
+  private
+
+  def post_params
+    params.permit(:title, :description, :price_visit, :area, :room)
+  end
+
 end
