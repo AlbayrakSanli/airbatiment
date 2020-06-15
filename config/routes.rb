139a7c486849aecb305a394a19b4123c7bdf2a0e
Lebeil <liedel75@gmail.com>
2020-06-15 20:12:03 +0200

    add & fix modal navbar user/owner

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :owners, path: 'owners'
   devise_for :users, path: 'users'
   resources :houses
-  root "houses#index"
+  root 'pages#index'
   resources :teams, only: :index
 
   resources :users
@@ -18,6 +19,6 @@ Rails.application.routes.draw do
   resources :appointments
   resources :charges
 
-  get "teams/test"
+  get 'teams/test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
