a8557adc76ee2d0b28f3a0869fcd1075dadb42bf
Albayrak Furkan <albayrak.mo@hotmail.fr>
2020-06-16 12:46:25 +0200

    jumbotron

--- config/routes.rb
+++ config/routes.rb
@@ -19,11 +19,7 @@ Rails.application.routes.draw do
   resources :appointments
   resources :charges
 
-<<<<<<< HEAD
   get "teams/test"
   get 'search', to: "houses#search"
-=======
-  get 'teams/test'
->>>>>>> 139a7c486849aecb305a394a19b4123c7bdf2a0e
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
